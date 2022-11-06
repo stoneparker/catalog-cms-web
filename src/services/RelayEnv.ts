0e54bb44d349a4283b46e4fa0fd1be59e5614e6e
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-11-06 20:22:24 -0300

    refactor: user token to relay fetch

--- src/services/RelayEnv.ts
+++ src/services/RelayEnv.ts
@@ -1,12 +1,13 @@
 import { Environment, Network, RecordSource, Store, FetchFunction } from 'relay-runtime';
+import { store } from '../reducers/store';
 
 const fetchRelay: FetchFunction = async (query, variables) => {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzY2YjE2NzQ2Y2MwZWIzZWExZjE5ZDAiLCJlbWFpbCI6ImFhQGlvay5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRkMUxRSEJJOWV6bkQxLzIxcmtqWHNPQ1JrWFkyWTY0UFROVEdiSUp0VkFIaHpHS3N2V3d3dSIsIm5hbWUiOiJvaSIsImNyZWF0ZWRBdCI6IjIwMjItMTEtMDVUMTg6NTQ6MzEuODUwWiIsInVwZGF0ZWRBdCI6IjIwMjItMTEtMDVUMTg6NTQ6MzEuODUwWiIsIl9fdiI6MCwiaWF0IjoxNjY3Njk4Njk1LCJleHAiOjE2Njg5MDgyOTV9.kWwp5rSbyYSEBcLclH5hVZERpvb9ky9MHCx7hq9qd3E';
+  const { user } = store.getState().auth;
 
   const response = await fetch('http://localhost:4000', {
     method: 'POST',
     headers: {
-      Authorization: token,
+      Authorization: user?.token || '',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
