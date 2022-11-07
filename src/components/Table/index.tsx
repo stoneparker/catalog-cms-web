07518d832c9605232b6fa4bcda4601241761a4c1
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-11-06 22:52:04 -0300

    fix: product description

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -80,7 +80,7 @@ const Table: React.FC<Props> = ({ editProduct, queryReference, loadQuery, produc
       render: (_, record) => (
         <ProductDetail
           imageUrl={record.imageUrl}
-          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+          description={record.description}
           name={record.name}
         />
       ),
