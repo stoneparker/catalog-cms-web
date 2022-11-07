372861c74cd186e8b63c840ce9f49bb63a820be1
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-11-07 11:42:27 -0300

    refactor: input height

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import { Input } from 'antd';
 
 export const Component = styled(Input)`
+  &, .ant-input {
+    min-height: 40px;
+  }
+
   span.ant-input-prefix {
     color: #b6b6b6;
   }
